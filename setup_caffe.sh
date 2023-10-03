8e15fc6001c835b8cd7a0e2d8440de2d00d8a1ca
Mathis C <mathis.cabrit@azuria.earth>
2023-10-03 09:52:22 +0200

    add some change

---
+++ setup_caffe.sh
@@ -0,0 +1,9 @@
+pip install --upgrade "pip < 21.0"
+cd python && for req in $(cat requirements.txt) pydot; do pip install $req; done && cd .. &&\
+mkdir build && cd build && \
+cmake -DCPU_ONLY=1 .. && \
+make -j"$(nproc)"
+
+export PYCAFFE_ROOT=$CAFFE_ROOT/python
+export PYTHONPATH=$PYCAFFE_ROOT:$PYTHONPATH
+export PATH=$CAFFE_ROOT/build/tools:$PYCAFFE_ROOT:$PATH
